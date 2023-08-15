8eb90d43fd98d09870ec993fab47912b5f10ad56
Brianclikclak <brianclikclak@hotmail.com>
2023-08-15 11:31:05 +0200

    creating project whit springboot

---
+++ src/test/java/com/brianmenendezfullstack/cursospringboot/CursoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brianmenendezfullstack.cursospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
